4755806fb081078825b400cbf123db89685529dd
Virendra <chavanvirendra11@gmail.com>
2025-09-25 09:39:48 +0530

    update 2- basic nav footer comp

---
+++ js/comp.js
@@ -0,0 +1,15 @@
+
+    async function loadComponent(id, file) {
+      try {
+        const res = await fetch(file);
+        if (!res.ok) throw new Error(`Failed to fetch ${file}`);
+        const html = await res.text();
+        document.getElementById(id).innerHTML = html;
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
+    // Load Navbar and Footer
+    loadComponent("navbar", "../components/header.html");
+    loadComponent("footer", "../components/footer.html");
